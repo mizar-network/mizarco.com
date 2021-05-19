42134d12128cba69b51009818e1c3bc8b943646b
mizar-network <84186896+mizar-network@users.noreply.github.com>
2021-05-19 22:59:24 +0300

    Add assets/ favicon.png index.html

---
+++ assets/js/app.js
@@ -0,0 +1,31 @@
+//VANTA.NET({
+  //el: "#network",
+  //mouseControls: true,
+  //touchControls: true,
+  //gyroControls: false,
+  //minHeight: 200.00,
+  //minWidth: 200.00,
+  //scale: 1.00,
+  //scaleMobile: 1.00,
+  //color: 0xfff498,
+  //backgroundColor: 0x0c0c0c,
+  //points: 8.00,
+  //maxDistance: 25.00,
+  //spacing: 10.00
+//})
+
+VANTA.DOTS({
+  el: "#network",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  backgroundColor: 0x0c0c0c,
+  color: 0xfff498,
+  color2: 0xfff498,
+  size: 3.30,
+  spacing: 40.00
+})
